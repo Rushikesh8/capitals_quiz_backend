5958f66dfefcd50c29c44c555116993257827588
Rushikesh8 <rushikeshsakharwade@gmail.com>
2023-07-14 00:30:20 +0530

    added models

--- capitals/models.py
+++ capitals/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class BaseModel(models.Model):
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
+        
+class Country(BaseModel):
+    name = models.CharField(max_length=120)
+    capital = models.CharField(max_length=120)
